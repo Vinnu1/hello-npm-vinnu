6a1a419dd9c606d92037e9303c3fcd9a23457ad4
Vinayak Sinha <vinayaksinha@zoho.com>
2023-07-13 13:05:14 +0530

    initial commit, package created and published on npm

---
+++ index.js
@@ -0,0 +1,6 @@
+//returns multiple of 5 below 5000
+const multipleof5 = () => {
+    return 5 * Math.floor(Math.random() * 1000)
+}
+
+module.exports = multipleof5
---
+++ readme.md
@@ -0,0 +1,3 @@
+Making this package to learn how to create and use npm packages.
+
+#### Exports function which muliplies random number between 0 - 999 with 5
